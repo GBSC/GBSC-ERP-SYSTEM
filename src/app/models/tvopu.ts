ba48a5de4fe2403e31cfbdb8a36fc0743ced6757
Mujtaba <info@gbscinc.com>
2018-09-04 15:25:21 +0500

    embryolohy thawed model added

---
+++ src/app/models/tvopu.ts
@@ -0,0 +1,38 @@
+export class Tvopu
+{
+
+    TvopuId : number;
+
+    CycleNumber : number;
+
+    TreatmentNumber : number;
+
+    TimeStart : Date;
+
+    TimeFinish : Date;
+
+    IsActive : boolean;
+
+    Remarks : string;
+
+    PickoutCount : string;
+
+    PickupDate : Date;
+
+    FollicileAspiratedLeft : string;
+
+    FollicileAspiratedRight : string;
+
+    OociteCollectedLeft : string;
+
+    OociteCollectedRight : string;
+
+    PackageId : number;
+
+    ConsultantId : number;
+
+    PatientId : number;
+
+    EmbryologistId : number;
+    
+}

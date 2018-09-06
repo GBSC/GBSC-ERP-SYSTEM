264826217fe162d37419b1d727af1b96ff8dc3ca
Mujtaba <info@gbscinc.com>
2018-09-06 11:19:34 +0500

    conflicts resolved

--- src/app/theme/theme-routing.module.ts
+++ src/app/theme/theme-routing.module.ts
@@ -10,21 +10,9 @@ import { LabModule } from '../../app/hims/lab/lab.module'
 import { SystemAdministrationModule } from '../systemadministration/systemadministration.module';
 import { HrmModule } from '../hrm/hrmsSetup/hrm.module';
 import { EmployeeModule } from '../hrm/employee/employee.module';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { PayrollModule } from '../payroll/payroll.module';
 import { SuperadminModule } from '../superadmin/superadmin.module';
-=======
-=======
-import { PayrollModule } from '../payroll/payroll.module';
->>>>>>> 2873d75f0588d0f5c48cb7e8d2c8e598f86fb1d9
 import { AccountModule } from '../account/account.module';
->>>>>>> 975215233674f410f72b5abe4e7de5f1d85e897e
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 2873d75f0588d0f5c48cb7e8d2c8e598f86fb1d9
 
 const routes: Routes = [
     {
@@ -74,17 +62,9 @@ const routes: Routes = [
         SystemAdministrationModule,
         HrmModule,
         EmployeeModule,
-<<<<<<< HEAD
-<<<<<<< HEAD
-        PayrollModule
-=======
-        AccountModule
->>>>>>> 975215233674f410f72b5abe4e7de5f1d85e897e
-=======
         AccountModule,
         PayrollModule
->>>>>>> 2873d75f0588d0f5c48cb7e8d2c8e598f86fb1d9
-    ],
+        ],
     exports: [
         RouterModule,
     ]

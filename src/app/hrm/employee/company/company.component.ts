1b1a018c8d17a9386770957c6ccb4bc341278bf7
Ovais <ovais.khan@gbscinc.com>
2018-10-03 05:12:15 -0700

    asd

--- src/app/hrm/employee/company/company.component.ts
+++ src/app/hrm/employee/company/company.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { SetupService, EmployeeService } from '../../../core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-employeecompany',
@@ -10,11 +11,38 @@ import { SetupService, EmployeeService } from '../../../core';
 export class EmployeeCompanyComponent implements OnInit {
 
     @Output('setCompanyFormValue') setCompanyFormValue = new EventEmitter();
-
+   
     public EmpCompanyForm: any;
-    constructor(public fb: FormBuilder, private SetupServiceobj: SetupService, public employee: EmployeeService) { }
 
-    async ngOnInit() {
+    public id : any;
+    public Employee : any;
+    constructor(public fb: FormBuilder, private SetupServiceobj: SetupService, public employee: EmployeeService,  public router: Router, private route: ActivatedRoute) { }
+
+async ngOnInit() {
+
+        console.log(this.router.url);
+
+        this.route.params.subscribe((params) => {
+            this.id = +params['id'];
+
+                    this.employee.GetEmployee(this.id).subscribe((Employee) => {
+                       this.Employee = Employee
+                      //  let emp = this.Employee;
+                            
+                      
+                           console.log(Employee)
+
+                            
+                    });
+
+
+                    
+                    
+
+        
+
+        });
+
         await this.SetupServiceobj.getAllFunctions();
         let fnc = this.SetupServiceobj.function;
 
@@ -51,6 +79,12 @@ export class EmployeeCompanyComponent implements OnInit {
     async addcompanyinfo() {
         let cmp = await this.employee.addusercompany();
         console.log(cmp);
+    }
+    
+    async update(value)
+    {
+        let x = await this.employee.updateUserCompanyById(value);
+        console.log(x);
     }
 
 

823e8bcc7441e7480bfacc5d97000264c1fa2df4
Abdul Wasay <Abdul Wasay@DESKTOP-H5I7CV3>
2018-10-03 18:23:02 +0500

    PharmacyStarted

---
+++ src/app/pharmacy/Setup/unit/unit.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { InventorysystemService } from '../../../core';
+
+
+@Component({
+    selector: 'app-unit',
+    templateUrl: './unit.component.html',
+    styleUrls: ['./unit.component.css']
+})
+export class UnitComponent implements OnInit {
+    private Units : any;
+    private UpdatedModel : any;
+
+    constructor(private InventoryService : InventorysystemService) {
+
+    }
+
+    async ngOnInit() {
+       this.Units = await this.InventoryService.GetUnits();
+    }
+
+    async AddUnit(value) {
+        await this.InventoryService.AddUnit(value.data);
+        this.Units = await this.InventoryService.GetUnits();
+    }
+
+    UpdateModel(value) {
+        this.UpdatedModel = {...value.oldData, ...value.newData};
+    }
+
+    async UpdateUnit() {
+        return await this.InventoryService.UpdateUnit(this.UpdatedModel);
+    }
+
+    async DeleteUnit(value) {
+        return await this.InventoryService.DeleteUnit(value.key);
+    }
+
+}

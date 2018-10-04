6863532f0b006e4af88c2c9366503bdf2cea26e7
Abdul Wasay <Abdul Wasay@DESKTOP-H5I7CV3>
2018-10-04 19:43:45 +0500

    InventoryItem

--- src/app/pharmacy/Setup/inventory-item/inventory-item.component.ts
+++ src/app/pharmacy/Setup/inventory-item/inventory-item.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { PharmacyService } from '../../../core';
+import { InventoryItem } from '../../../core/Models/Pharmacy/InventoryItem';
+import { Unit } from '../../../core/Models/Pharmacy/Unit';
+import { PackType } from '../../../core/Models/Pharmacy/PackType';
+import { PackSize } from '../../../core/Models/Pharmacy/PackSize';
+import { PackCategory } from '../../../core/Models/Pharmacy/PackCategory';
+import { ProductType } from '../../../core/Models/Pharmacy/ProductType';
+import { InventoryItemCategory } from '../../../core/Models/Pharmacy/InventoryItemCategory';
+import { PackageType } from '../../../core/Models/Pharmacy/PackageType';
 
 
 
@@ -10,10 +18,9 @@ import { PharmacyService } from '../../../core';
 })
 
 export class InventoryItemComponent implements OnInit {
-    private InventoryItems : any;
-    private Brands : any;
-    private Units : any;
-    private PackTypes : any;
+    private InventoryItems : InventoryItem;
+    private Units : Unit;
+    private PackTypes : PackType;
     private PackSizes : any;
     private PackCategories : any;
     private ProductTypes : any;
@@ -25,20 +32,28 @@ export class InventoryItemComponent implements OnInit {
 
     }
 
-    async ngOnInit() {
-        this.InventoryItems = await this.PharmacyService.GetInventoryItems();
-        this.Units = await this.PharmacyService.GetUnits();
-        this.PackTypes = await this.PharmacyService.GetPackTypes();
-        this.PackSizes = await this.PharmacyService.GetPackSizes();
-        this.PackCategories = await this.PharmacyService.GetPackCategories();
-        this.ProductTypes = await this.PharmacyService.GetProductTypes();
-        this.InventoryItemCategories = await this.PharmacyService.GetInventoryItemCategories();
-        this.PackageTypes = await this.PharmacyService.GetPackageTypes();
+    ngOnInit() {
+        this.PharmacyService.GetInventoryItems().subscribe((res : InventoryItem) => this.InventoryItems = res);
+        this.PharmacyService.GetUnits().subscribe((res : Unit) => this.Units = res);
+        this.PharmacyService.GetPackTypes().subscribe((res : PackType) => this.PackTypes = res);
+        this.PharmacyService.GetPackSizes().subscribe((res : PackSize) => this.PackSizes.push(res));
+        this.PharmacyService.GetPackCategories().subscribe((res : PackCategory) => this.PackCategories.push(res));
+        this.PharmacyService.GetProductTypes().subscribe((res : ProductType) => this.ProductTypes.push(res));
+        this.PharmacyService.GetInventoryItemCategories().subscribe((res : InventoryItemCategory) => this.InventoryItemCategories.push(res));
+        this.PharmacyService.GetPackageTypes().subscribe((res : PackageType) => this.PackageTypes.push(res));
+        console.log("InventoryItems", this.InventoryItems);
+        console.log("Units", this.Units);
+        console.log("PackTypes", this.PackTypes);
+        console.log("PackSizes", this.PackSizes);
+        console.log("PackCategories", this.PackCategories);
+        console.log("ProductTypes", this.ProductTypes);
+        console.log("InventoryItemCategories", this.InventoryItemCategories);
+        console.log("PackageTypes", this.PackageTypes);
     }
 
     async AddInventoryItem(value) {
         await this.PharmacyService.AddInventoryItem(value.data);
-        this.InventoryItems = await this.PharmacyService.GetInventoryItems();
+        //this.PharmacyService.GetInventoryItems().subscribe(res => this.InventoryItems = res);
     }
 
     UpdateModel(value) {
--- src/app/pharmacy/purchase-order/purchase-order.component.ts
+++ src/app/pharmacy/purchase-order/purchase-order.component.ts
@@ -11,25 +11,34 @@ import { PharmacyService } from '../../core';
 })
 export class PurchaseOrderComponent implements OnInit {
 
-    private PurchaseOrder: any;
-    private Supplier: any;
+    private PurchaseOrders: any;
+    private Suppliers: any;
     private User: any;
+    private UpdatedModel : any;
+    private PurchaseOrderForm : FormGroup;
 
-    constructor(private PharmacyService: PharmacyService) {
-
+    constructor(private PharmacyService: PharmacyService, private FormBuilder : FormBuilder) {
+        this.PurchaseOrderForm = this.FormBuilder.group( {} );
     }
 
-    async ngOnInit() {
-        this.Supplier = await this.PharmacyService.GetSuppliers();
-        this.PurchaseOrder = await this.PharmacyService.GetPurchaseOrders();
+    ngOnInit() {
+        this.PharmacyService.GetSuppliers().subscribe(res => this.Suppliers = res);
+        this.PharmacyService.GetPurchaseOrders().subscribe(res => this.PurchaseOrders = res);
+        console.log(this.PurchaseOrders);
+        console.log(this.Suppliers);
     }
 
     async AddPurchaseOrder(value) {
         await this.PharmacyService.AddPurchaseOrder(value);
+        this.PurchaseOrders = await this.PharmacyService.GetPurchaseOrders();
     }
 
-    async UpdatePurchaseOrder(value) {
-        await this.PharmacyService.UpdatePurchaseOrder(value.key);
+    UpdateModel(value) {
+        this.UpdatedModel = { ...value.oldData, ...value.newData };
+    }
+
+    async UpdatePurchaseOrder() {
+        await this.PharmacyService.UpdatePurchaseOrder(this.UpdatedModel);
     }
 
     async DeletePurchaseOrder(value) {

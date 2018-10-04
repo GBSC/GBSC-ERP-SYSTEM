46dc8344966fe7d5a19c8c34b90e4252ea882c51
Mujtaba <info@gbscinc.com>
2018-10-05 00:17:03 +0500

    basic information form work in progress

--- src/app/hrm/employee/basicinformation/basicinformation.component.ts
+++ src/app/hrm/employee/basicinformation/basicinformation.component.ts
@@ -13,14 +13,14 @@ export class BasicinformationComponent implements OnInit {
 
 
     public basic: any;
-    public Employee : Employee;
+    public Employee: Employee;
 
-    @Input('id') id : number;
+    @Input('id') id: number;
     @Output('setbasicinfoFormValue') setBasicinfoFormValue = new EventEmitter();
 
     public EmpbasicForm: FormGroup;
 
-    constructor(public employeeService: EmployeeService, public fb: FormBuilder, private SetupServiceobj: SetupService,  public router: Router, private route: ActivatedRoute) { 
+    constructor(public employeeService: EmployeeService, public fb: FormBuilder, private SetupServiceobj: SetupService, public router: Router, private route: ActivatedRoute) {
 
         this.EmpbasicForm = this.fb.group({
             FirstName: [''],
@@ -29,14 +29,14 @@ export class BasicinformationComponent implements OnInit {
             Email: [''],
             Cnic: [''],
             CnicExpiry: [''],
-            PhoneNumber: [''],
-            HomeNumber: [''],
+            Phone: [''],
+            HomePhone: [''],
             DOB: [''],
             POB: [''],
             BloodGroup: [''],
             MaritalStatus: [''],
             Gender: [''],
-            countryId: [''],
+            CountryId: [''],
             CityId: [''],
             ReligionId: [''],
             LanguageId: [''],
@@ -45,120 +45,76 @@ export class BasicinformationComponent implements OnInit {
         });
 
     }
-    public basicInfo: any = {};
-
-    // async ngOnInit() {
-
-    //     this.basicInfo = await this.employee.getBasicInfoOfCurrentUser();
-    //     console.log(this.basicInfo);
-    //     await this.employee.GetAllEmployees();
-    //     this.basic = this.employee.employeereg;
-    //     this.employee.EmpbasicForm.value.FirstName = this.basicInfo.firstName;
-    //     this.employee.EmpbasicForm.value.LastName = this.basicInfo.lastName;
-
-    //     console.log(this.employee.EmpbasicForm.value);
-
-    //     await this.SetupServiceobj.getAllDesignations();
-    //     let dsg = this.SetupServiceobj.designation;
-
-    //     await this.SetupServiceobj.getAllLanguages();
-    //     let lng = this.SetupServiceobj.language;
-
-
-    //     await this.SetupServiceobj.getAllFunctions();
-    //     let func = this.SetupServiceobj.function;
-
-    //     await this.SetupServiceobj.getAllReligions();
-    //     let relg = this.SetupServiceobj.religion;
-    //     console.log(relg);
-
-    //     await this.SetupServiceobj.getAllGazettedHolidays();
-    //     let holiday = this.SetupServiceobj.gazetholidays;
-
-    //     await this.SetupServiceobj.getAllCities();
-    //     let cty = this.SetupServiceobj.city;
-
-    // }
 
     getbasicinfoFormValue() {
         this.setBasicinfoFormValue.emit(this.EmpbasicForm.value);
     }
 
-    // async onsubmit() {
-    //   console.log(this.activatedRoute.url);
-    //   // let basicinfo = await this.employee.addEmployee();
-    //   // console.log(basicinfo);
-    //   // this.router.navigate(['employees']);
-
-    //   // if (basicinfo){
-    //   //   } 
-    //   //   else{
-    //   //     alert('User could not created');
-
-    //   //   }
-    // }
-    async update(value){
+    async update(value) {
         console.log(value);
-    //    this.employeeService.EmpbasicForm.value.userId = this.id;
-    //         // value = {...this.Employee, ...value}
-    //        // this.joinValues(value, this.Employee);
-    //      await this.employeeService.updateEmployee();
+
     }
 
-    async updateUersById(value)
-    {
+    async updateUersById(value) {
         console.log(value);
         await this.employeeService.updateUersById(value);
     }
 
-    // joinValues(form, data) {
-    //     console.log(data)
-      
-    //     for(let k in data) {
-    //         for(let p in form) {
-    //             p = p.substr(0, 1).toLowerCase() + p.substr(1);
-    //             // console.log(p);
-    //             if(p === k) {
-    //                 console.log('--------------')
-    //                 console.log('fro,m join values', k, data[k], p, form[p]);
-    //                 // console.log(form)
-    //                 console.log('--------------')
-    //             }
-    //         }
-    //     }
-    // }
-
     async ngOnInit() {
 
-        this.employeeService.GetEmployee(this.id).subscribe(resp=>{
+        this.employeeService.GetEmployee(this.id).subscribe(resp => {
 
             this.Employee = resp;
 
-            console.log(this.Employee);
+            this.patchValues(resp);
+
         });
 
 
-    
-            await this.SetupServiceobj.getAllDesignations();
-            let dsg = this.SetupServiceobj.designation;
-    
-            await this.SetupServiceobj.getAllLanguages();
-            let lng = this.SetupServiceobj.language;
-    
-    
-            await this.SetupServiceobj.getAllFunctions();
-            let func = this.SetupServiceobj.function;
-    
-            await this.SetupServiceobj.getAllReligions();
-            let relg = this.SetupServiceobj.religion;
-            console.log(relg);
-    
-            await this.SetupServiceobj.getAllGazettedHolidays();
-            let holiday = this.SetupServiceobj.gazetholidays;
-    
-            await this.SetupServiceobj.getAllCities();
-            let cty = this.SetupServiceobj.city;
 
- 
+        await this.SetupServiceobj.getAllDesignations();
+        let dsg = this.SetupServiceobj.designation;
+
+        await this.SetupServiceobj.getAllLanguages();
+        let lng = this.SetupServiceobj.language;
+
+
+        await this.SetupServiceobj.getAllFunctions();
+        let func = this.SetupServiceobj.function;
+
+        await this.SetupServiceobj.getAllReligions();
+        let relg = this.SetupServiceobj.religion;
+        console.log(relg);
+
+        await this.SetupServiceobj.getAllGazettedHolidays();
+        let holiday = this.SetupServiceobj.gazetholidays;
+
+        await this.SetupServiceobj.getAllCities();
+        let cty = this.SetupServiceobj.city;
+
+
+    }
+
+    patchValues(employee : any) {
+
+        this.EmpbasicForm.patchValue({
+            FirstName: employee.firstName,
+            LastName: employee.lastName,
+            FatherName: employee.fatherName,
+            Email: employee.email,
+            Cnic: employee.cnic,
+            CnicExpiry: employee.cnicExpiry,
+            Phone: employee.phone,
+            HomePhone: employee.homePhone,
+            DOB: employee.dob,
+            POB: employee.pob,
+            BloodGroup: employee.bloodGroup,
+            MaritalStatus: employee.maritalStatus,
+            Gender: employee.gender,
+            CityId: employee.cityId,
+            ReligionId: employee.religionId,
+            Address: employee.address,
+            PermanentAddress: employee.permanentAddress
+        });
     }
 }

9d133445bedcdf4a2ec6cdea3e7576440d1e5730
Abdul Wasay <Abdul Wasay@DESKTOP-H5I7CV3>
2018-10-05 18:00:30 +0500

    PharmacyServiceGetByCodeMethodsAdded

---
+++ src/app/pharmacy/grn-view/grn-view.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { GRN } from '../../core/Models/Pharmacy/GRN';
+import { PharmacyService } from '../../core';
+
+@Component({
+  selector: 'app-grn-view',
+  templateUrl: './grn-view.component.html',
+  styleUrls: ['./grn-view.component.scss']
+})
+export class GrnViewComponent implements OnInit {
+
+  private GRNs : GRN;
+  private DetailGRN : GRN;
+
+  constructor(private PharmacyService : PharmacyService) {
+
+  }
+
+  ngOnInit() {
+    this.PharmacyService.GetGRN().subscribe((res : GRN) => this.GRNs = res);
+  }
+
+  GetGrnDetails(value) {
+    this.PharmacyService.GetGrnDetailsByCode(value.data.grnNumber).subscribe((res : GRN) => this.DetailGRN = res);
+  }
+
+}

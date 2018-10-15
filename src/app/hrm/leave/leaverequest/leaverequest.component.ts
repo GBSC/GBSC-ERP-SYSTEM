4d8442aca770a2512b69e4137f7ea73d0df43f80
GBSC-5 <zeeshan.akhtar@gbscinc.com>
2018-10-15 13:16:28 +0500

    changes Applied

--- src/app/hrm/leave/leaverequest/leaverequest.component.ts
+++ src/app/hrm/leave/leaverequest/leaverequest.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { LeaveSetupService, EmployeeService, LeaveService } from '../../../core';
 import { Router } from '@angular/router';
+import { LeaveRequestDetail } from '../../../core/Models/HRM/leaveRequestDetail';
+import { LeaveRequest } from '../../../core/Models/HRM/leaveRequest';
 
 @Component({
     selector: 'app-leaverequest',
@@ -18,58 +20,65 @@ export class LeaverequestComponent implements OnInit {
     public leaveType: any;
     public msg: any;
     public leaveApprovr: any;
-    public combinedData = {};
-    leaverequest: any;
+    public leaverequest: any;
+    public updatingRequest: any;
+    public leaveOpening: any;
     public leaveRequestId;
+    private requestDetail: LeaveRequestDetail[];
 
     constructor(private fb: FormBuilder, public leavesetupservice: LeaveSetupService, public empservice: EmployeeService,
         public router: Router, public leaveservice: LeaveService) { }
 
     async ngOnInit() {
 
-        this.leaveRequestForm = this.fb.group({
-            UserId: ['', Validators]
-        });
+        this.requestDetail = [];
 
-        this.leaveRequestDetailForm = this.fb.group({
-            LeaveYearId: ['', Validators],
-            LeaveTypeId: ['', Validators],
-            DateFrom: ['', Validators],
-            DateTill: ['', Validators],
-            Description: ['', Validators],
-            IsShortLeave: ['', Validators],
-            FirstSecondHalf: ['', Validators],
-            TotalLeaveValue: ['', Validators]
+        this.leaveRequestForm = this.fb.group({
+            UserId: ['', Validators],
+            IsApproved: ['', Validators],
+            RequestDate: ['', Validators]
         });
 
 
         this.leaverequestdetail = await this.leaveservice.getLeaveRequestDetails();
-         
-        await this.leaveservice.getAllleaverequest(); 
- 
+
+        this.leaverequest = await this.leaveservice.getAllleaverequest();
+
+        this.leaveOpening = await this.leaveservice.getLeaveOpening();
+
         this.employees = await this.empservice.GetAllEmployees();
 
         this.leaveApprovr = await this.leavesetupservice.getLeaveApprovers();
-       
-        this.leaveYear = await this.leavesetupservice.getLeaveYears();
-   
-        this.leaveType =await this.leavesetupservice.getLeaveTypes();
-     }
 
-    async addleaverequestordetail() {
-        this.leaveRequestDetailForm.value.leaveRequestId = this.leaveRequestId.leaverequestID;
-        await this.leaveservice.addLeaveRequestDetail(this.leaveRequestDetailForm.value);
-        this.msg = 'Success! Request Detail Save Successfully';
+        this.leaveYear = await this.leavesetupservice.getLeaveYears();
+
+        this.leaveType = await this.leavesetupservice.getLeaveTypes();
+    }
+
+    async leaveRequestDetail(value) {
+        let data = value.data;
+        this.requestDetail.push(data);
+    }
+
+    async updatingRequestDetail(value) {
+        this.updatingRequest = { ...value.oldData, ...value.newData };
+    }
+
+    async updateRequestDetail() {
+        await this.leaveservice.updateLeaveRequestDetail( this.updatingRequest);
+    }
+
+
+    async addLeaveRequest(value) {
+        let request = new LeaveRequest();
+        request = { ...request, ...value };
+        request.LeaveRequestDetails = this.requestDetail;
+        let s = await this.leaveservice.addLeaveRequest(request);
+        this.msg = 'Success! Leave Request Submit Successfully';
         setTimeout(() => {
             this.msg = null;
-          }, 3000);
+        }, 3000);
+        this.leaveRequestForm.reset();
     }
-
-
-    async addleaverequest(e) { 
-        this.leaveRequestId = await this.leaveservice.addLeaveRequest(this.leaveRequestForm.value);
-    }
-
-
-
+ 
 }

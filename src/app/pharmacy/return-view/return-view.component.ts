9b6d596454febbee49b41cec2d740626900d2ea4
Ovais <ovais.khan@gbscinc.com>
2018-10-17 07:01:43 -0700

    return  view

--- src/app/pharmacy/return-view/return-view.component.ts
+++ src/app/pharmacy/return-view/return-view.component.ts
@@ -18,13 +18,15 @@ export class ReturnViewComponent implements OnInit {
     ngOnInit() {
         this.PharmacyService.GetSalesReturns().subscribe(res => {
             console.log(res);
-
             this.SalesReturns = res;
             console.log(this.SalesReturns);
-
         });
+    
+    
+    
     }
 
 
 
 }
+

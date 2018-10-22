19b5e639e6230f22fa4e2b66788c3bf6b92806e2
kaleemshk <kalim29746@gmail.com>
2018-10-21 23:21:20 -0700

    update ui dashboard

--- src/app/dashboard/root/root.component.ts
+++ src/app/dashboard/root/root.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
 import { ScriptLoaderService } from '../../_services/script-loader.service';
-import { Router } from '@angular/router';
 import { Helpers } from '../../helpers';
 
+declare let mApp: any;
+declare let mUtil: any;
+declare let mLayout: any;
 
 @Component({
     selector: 'app-root',
@@ -16,19 +19,33 @@ export class RootComponent implements OnInit {
     constructor(private _script: ScriptLoaderService, private _router: Router) { }
 
     ngOnInit() {
-        this._script.loadScripts('body', ['assets/vendors/base/vendors.bundle.js', 'assets/demo/default/base/scripts.bundle.js', 'assets/app/js/dashboard-erp.js'], true)
+        this._script.loadScripts('body', true)
             .then(result => {
                 Helpers.setLoading(false);
                 // optional js to be loaded once
-                this._script.loadScripts('head', ['assets/vendors/custom/fullcalendar/fullcalendar.bundle.js'], true);
+                this._script.loadScripts('head', ['assets/vendors/custom/fullcalendar/fullcalendar.bundle.js']);
             });
-
-    }
-
-    ngAfterViewInit() {
-        this._script.loadScripts('body',
-            ['assets/demo/default/custom/components/forms/widgets/bootstrap-datepicker.js']);
-
+        this._router.events.subscribe((route) => {
+            if (route instanceof NavigationStart) {
+                (<any>mLayout).closeMobileAsideMenuOffcanvas();
+                (<any>mLayout).closeMobileHorMenuOffcanvas();
+                (<any>mApp).scrollTop();
+                Helpers.setLoading(true);
+                // hide visible popover
+                (<any>$('[data-toggle="m-popover"]')).popover('hide');
+            }
+            if (route instanceof NavigationEnd) {
+                // init required js
+                (<any>mApp).init();
+                (<any>mUtil).init();
+                Helpers.setLoading(false);
+                // content m-wrapper animation
+                let animation = 'm-animate-fade-in-up';
+                $('.m-wrapper').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(e) {
+                    $('.m-wrapper').removeClass(animation);
+                }).removeClass(animation).addClass(animation);
+            }
+        });
     }
 
 }

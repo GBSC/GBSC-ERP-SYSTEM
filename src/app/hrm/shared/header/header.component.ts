17a3d7a05be80e2f3abfd2b69f95066c2a67626b
kaleemshk <kalim29746@gmail.com>
2018-10-23 06:23:18 -0700

    update ui dashboRD

--- src/app/hrm/shared/header/header.component.ts
+++ src/app/hrm/shared/header/header.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit, ViewEncapsulation, AfterViewInit } from '@angular/core';
-import { Helpers } from '../../../helpers';
-import { AccountService } from '../../../core';
 
-
+declare let mLayout: any;
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-    constructor(private accountService: AccountService) { }
+export class HeaderComponent implements OnInit, AfterViewInit {
+    constructor() { }
 
     ngOnInit() {
     }
 
-    logout() {
-        //this.accountService.logout();
+    ngAfterViewInit() {
+
+        mLayout.initHeader();
+
     }
 
 }

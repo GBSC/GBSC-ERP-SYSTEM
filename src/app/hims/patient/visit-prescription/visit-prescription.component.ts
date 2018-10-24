ac732dd3168f8b8f3f3ddbe9c2132a2a12d5614b
wasaya2 <engr.s.a.wasay@gmail.com>
2018-10-24 18:30:40 +0500

    PrescriptionInProgress

--- src/app/hims/patient/visit-prescription/visit-prescription.component.ts
+++ src/app/hims/patient/visit-prescription/visit-prescription.component.ts
@@ -5,6 +5,7 @@ import { SalesIndentItem } from '../../../core/Models/Pharmacy/SalesIndentItem';
 import { Patient } from '../../../core/Models/HIMS/patient';
 import { ActivatedRoute } from '@angular/router';
 import { DxDataGridComponent } from 'devextreme-angular';
+import { SalesIndent } from '../../../core/Models/Inventory/Sales/SalesIndent';
 
 @Component({
   selector: 'app-visit-prescription',
@@ -24,15 +25,15 @@ export class VisitPrescriptionComponent implements OnInit {
 	private CurrentPatientID : number;
 	
 	private Prescriptions : any[] = [];
-	private UpdatedModel : SalesIndentItem;
+	private SalesIndentItems : SalesIndentItem[] = [];
+	
+	private TotalQuantity : number = 0;
+	private TotalCostPrice : number = 0;
+	private TotoalRetailPrice : number = 0;
+
+	private Index : number = 0;
 
 	constructor(private PharmacyService : PharmacyService, private PatientService : PatientService, private Router: ActivatedRoute) {
-		// this.setDescriptionValue = this.setDescriptionValue.bind(this);
-		// this.setPackTypeValue = this.setPackTypeValue.bind(this);
-		// this.setPackSizeValue = this.setPackSizeValue.bind(this);
-		// this.setDoasgeValue = this.setDoasgeValue.bind(this);
-		// this.getFilteredTerritories = this.getFilteredTerritories.bind(this);
-		this.onEditingStart = this.onEditingStart.bind(this);
 	}
 
 	async ngOnInit() {
@@ -40,177 +41,84 @@ export class VisitPrescriptionComponent implements OnInit {
 			this.InventoryItems = res;
 			this.FilteredInventoryItems = this.InventoryItems;
 			this.InventoryItemDataSource = this.InventoryItems;
-			// console.log("InventoryItems", this.InventoryItems);
-			// console.log("FilteredInventoryItems", this.FilteredInventoryItems);
-			// console.log("InventoryItemDataSource", this.InventoryItemDataSource);
 		});
 		
 		this.Router.params.subscribe(params => {
 			this.CurrentPatientID = params['id'];
 			this.PatientService.getpatient(this.CurrentPatientID).subscribe((Patient : Patient) => {
 				this.CurrentPatient = Patient;
-				//console.log("CurrentPatient", this.CurrentPatient);
 			});
 		});
 	}
 
-	onEditingStart(event) {
-		console.log(event);
-		// console.log("EditingStart");
-		// let cached = this.InventoryItemDataSource;
+	onEditorPreparing(e) {
 		this.InventoryItemDataSource = this.FilteredInventoryItems;
- 
-		// if (event.cancel === true){
-		// 	console.log("aaaaaa");
-			
-		// 	this.InventoryItemDataSource = cached;
-		// }
 	}
 
-	// onCancelEditData(event) {
-	// 	console.log(event);
+	addDays(date : Date, days : string) : Date {
+		console.log("Before : ", date);
+		date.setDate( date.getDate() + Number.parseInt(days) );
+		console.log("After : ", date);
 		
-	// }
-
-	// onCellPrepared(element : any, info : any) { 
-	// 	if (info.column.command === 'edit') 
-	// 		element.on('dxclick', '.dx-link', function() { 
-	// 			if ($(this).text() === 'Cancel') {
-
-	// 			};
-	// 		});
-	// }
+		return date;
+	}
 
 	AddPrescription(value) {
-		this.FilteredInventoryItems = this.FilteredInventoryItems.filter(a => a.inventoryItemId != value.data.inventoryItemId);
-		console.log("FilteredInventoryItems", this.FilteredInventoryItems);
-		this.InventoryItemDataSource = this.InventoryItems;
+		// console.log(value);
+		// value.data.treatmentEnd = this.addDays(<Date>value.data.treatmentStart, value.data.treatmentTimeInDays);
 		// console.log(value);
 		// console.log(this.Prescriptions);
-		// this.Prescriptions.push(value.data);
+		
+		console.log(value.data.treatmentStart);
+
+		this.SelectedInventoryItem = this.FilteredInventoryItems.find(a => a.inventoryItemId == value.data.inventoryItemId);
+		console.log("SelectedInventoryItem", this.SelectedInventoryItem);
+
+		var a : any = {
+			inventoryItemId : <number>value.data.inventoryItemId,
+			treatmentStart : value.data.treatmentStart,
+			treatmentTimeInDays : <number>value.data.treatmentTimeInDays,
+			treatmentEnd : this.addDays(value.data.treatmentStart, value.data.treatmentTimeInDays),
+			dosage : <number>value.data.dosage
+		};
+
+		console.log(a);
+		this.Prescriptions.push(a);
+		console.log(this.Prescriptions);
+		this.Prescriptions.splice(this.Index, 1);
+		console.log(this.Prescriptions);
+		// this.SalesIndentItems.push(a);
+		this.TotalQuantity += Number.parseInt(value.data.dosage) * Number.parseInt(value.data.treatmentTimeInDays);
+		this.TotalCostPrice += this.TotalQuantity * Number.parseFloat(this.SelectedInventoryItem.costPrice);
+		this.TotoalRetailPrice += this.TotalQuantity * Number.parseFloat(this.SelectedInventoryItem.retailPrice);
+		this.Index += 1;
+		
+		this.FilteredInventoryItems = this.FilteredInventoryItems.filter(a => a.inventoryItemId != value.data.inventoryItemId);
+		this.InventoryItemDataSource = this.InventoryItems;
+
 	}
 
-	// UpdateModel(value) {
-	// 	console.log(value);
-	// 	let b : any = this.InventoryItems;
-	// 	let a : InventoryItem = b.find( c => c.inventoryItemId == value.oldData.inventoryItemId );
-	// 	this.FilteredInventoryItems.push(a);
-	// 	// console.log("FilteredInventoryItems", this.FilteredInventoryItems);
-	// 	this.UpdatedModel = { ...value.oldData, ...value.newData };
-	// 	this.FilteredInventoryItems = this.FilteredInventoryItems.filter(a => a.inventoryItemId != value.newData.inventoryItemId);
-	// 	// console.log("FilteredInventoryItems", this.FilteredInventoryItems);
-	// }
-
-	// UpdatePrescription() {
-	// 	// console.log(this.Prescriptions);
-	// 	this.InventoryItemDataSource = this.InventoryItems;
-	// }
-
 	DeletePrescription(value) {
+		console.log(value);
 		let b : any = this.InventoryItems;
 		let a : InventoryItem = b.find( c => c.inventoryItemId == value.data.inventoryItemId );
 		this.FilteredInventoryItems.push(a);
-		// console.log("FilteredInventoryItems", this.FilteredInventoryItems);
-		// console.log(value);
-		// console.log(this.Prescriptions);
 	}
-
-	// setDescriptionValue(rowData: any, value: any): void {
-	//   console.log("DescriptionValue", value, rowData);
-	// }
-
-	// // setTerritoryValue(rowData: any, value: any): void {
-	// //   rowData.territoryId = null;
-	// //   (<any>this).defaultSetCellValue(rowData, value);
-	// // }
-
-	// setPackTypeValue(rowData: any, value: any): void {
-	//   console.log("PackTypeValue", value, rowData);
-	// }
-
-	// setPackSizeValue(rowData: any, value: any): void {
-	//   console.log("PackSizeValue", value, rowData);
-	// }
-
-	// setDoasgeValue(rowData: any, value: any): void {
-	//   console.log("DoasgeValue", value, rowData);
-	// }
-
-	// setItemValue(rowData: any, value: any) : void {
-	//   //console.log("ItemValue", value, rowData);
-	//   //rowData.areaId = null;
-	//   (<any>this).defaultSetCellValue(rowData, value);
-	//   console.log("AAAAAAAAA");
-	//   // (<any>this).editorOptions.DescriptionValue = "ASDFGHJKL";
-	// }
-
-	// setItemValue(event) : void {
-	//   //console.log("ItemValue", value, rowData);
-	//   //rowData.areaId = null;
-	//   //(<any>this).defaultSetCellValue(rowData, value);
-	//   console.log("AAAAAAAAA");
-	//   // (<any>this).editorOptions.DescriptionValue = "ASDFGHJKL";
-	// }
 
 	setItemValue(rowData: any, value: any): void {
 		rowData.inventoryItemId = null;
 		(<any>this).defaultSetCellValue(rowData, value);
 	}
 
-	// onCellPrepared(e) {
-	// 	// console.log(e);
-	// 	if (e.rowType === 'data' && e.column.command === 'edit') {
-	// 		console.log(e);
-	// 		//this.gridContainer.instance.refresh();
-	// 		// console.log("InventoryItemDataSource", this.InventoryItemDataSource);
-	// 		// this.InventoryItemDataSource = this.InventoryItems;
-	// 		e.element.oncancel = function() {
-	// 			console.log("a");
-	// 			this.InventoryItemDataSource = this.InventoryItems;
-	// 		};
+	onSubmit() {
+		var a : any = {
+			date : new Date(),
+			// totalTradePrice : 
+		};
 
-	// 		// if(e.cellElement == 'click' || e.cellElement == '.dx-link-cancel') {
-	// 		// 	console.log("a");
-	// 		// 	this.InventoryItemDataSource = this.InventoryItems;
-	// 		// }
-	// 		// e.cellElement.oncancel('click', '.dx-link-cancel', function() { 
-	// 		// 	alert('cancelled');
-	// 		// 	this.InventoryItemDataSource = this.InventoryItems;
-	// 		//  });
-	// 	}
-		
-	// }
-
-	onEditorPreparing(e) {
-		console.log(e);
-		if (e.parentType === "dataRow" && e.value && e.dataField === "inventoryItemId") {
-			// console.log(e);
-			this.SelectedInventoryItem = this.FilteredInventoryItems.find(a => a.inventoryItemId == e.value);
-			this.InventoryItemDataSource = this.InventoryItems;
-			// console.log("this.SelectedInventoryItem", this.SelectedInventoryItem);
-		}
-		// if (e.parentType === "dataRow" && e.dataField === "distributorId") {
-		//     e.editorOptions.disabled = (typeof e.row.data.territoryId !== "number");
-		// }
-		// console.log(e);
 	}
 
-	// getFilteredTerritories(options) {
-	//   return {
-	//       store: this.InventoryItems,
-	//       filter: options.data ? ["inventoryItemId", "=", options.data.inventoryItemId] : null
-	//   };
-	// }
+	onReset() {
 
-	// onEditorPreparing(e) {
-	//   if (e.parentType === "dataRow" && e.dataField === "inventoryItemId") {
-	//       e.editorOptions.disabled = (typeof e.row.data.inventoryItemId !== "number");
-	//   }
-	//   console.log("onEditorPreparing", e);
-	// }
-
-	// onEditorPrepared (e) {
-	//   console.log("onEditorPrepared", e);
-	// }
-
+	}
 }

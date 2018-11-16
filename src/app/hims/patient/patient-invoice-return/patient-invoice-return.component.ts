716e791ce0a0e9043b97041df41df12ab67bc1bd
wasaya2 <engr.s.a.wasay@gmail.com>
2018-11-16 17:08:35 +0500

    InvReturnInit

---
+++ src/app/hims/patient/patient-invoice-return/patient-invoice-return.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-invoice-return',
+  templateUrl: './patient-invoice-return.component.html',
+  styleUrls: ['./patient-invoice-return.component.scss']
+})
+export class PatientInvoiceReturnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

6ab7de55cc6e90b90b25ebd61721f6e945a97df4
kaleemshk <kalim29746@gmail.com>
2018-12-18 12:35:25 +0500

    update foam

--- src/app/etracker/territories/distributers/distributers.component.ts
+++ src/app/etracker/territories/distributers/distributers.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewEncapsulation, AfterViewInit } from '@angular/core';
+import { ScriptLoaderService } from '../../../_services/script-loader.service';
 @Component({
   selector: 'app-distributers',
   templateUrl: './distributers.component.html',
-  styleUrls: ['./distributers.component.scss']
+  styleUrls: ['./distributers.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class DistributersComponent implements OnInit {
+export class DistributersComponent implements OnInit, AfterViewInit{
+  
 
-  constructor() { }
+  constructor(public _script: ScriptLoaderService) {
+
+  }
 
   ngOnInit() {
   }
+
+  ngAfterViewInit() {
+    this._script.loadScripts('app-distributers',
+        ['assets/demo/default/custom/components/forms/widgets/select2.js']);
+
+}
 
 }

99b9c3e7a37827d3a364455cf4fed927b92c3b8a
Mujtaba <info@gbscinc.com>
2019-01-10 17:14:57 +0500

    update

--- src/app/inventory/setup/inventory-master/inventory-master.component.ts
+++ src/app/inventory/setup/inventory-master/inventory-master.component.ts
@@ -8,7 +8,7 @@ import { SystemAdministrationService } from '../../../../../src/app/core';
 })
 export class InventoryMasterComponent implements OnInit {
 
-    tabs: string[] = ['product-type', 'brand', 'product-category', 'unit', 'pack-category', 'pack-type', 'pack-size', 'package-type', 'inventory'];
+    tabs: string[] = ['product-type', 'brand', 'product-category', 'unit', 'pack-category', 'pack-type', 'pack-size', 'oil-type', 'products', 'general-sku'];
     selectedTab = this.tabs[0];
 
     constructor() { }

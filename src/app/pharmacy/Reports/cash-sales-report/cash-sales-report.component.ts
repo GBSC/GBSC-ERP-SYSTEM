d68f32d585717af04a7909ff11c771280dc5f745
Ovais <ovais.khan@gbscinc.com>
2019-02-08 12:32:19 +0500

    udpate

---
+++ src/app/pharmacy/Reports/cash-sales-report/cash-sales-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cash-sales-report',
+  templateUrl: './cash-sales-report.component.html',
+  styleUrls: ['./cash-sales-report.component.css']
+})
+export class CashSalesReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

9b29e84c4a675f44b9c24d4b867718a81e4d0a33
Ovais <ovais.khan@gbscinc.com>
2019-02-26 13:36:03 +0500

    update

--- src/app/hims/lab/report-components/biochemistry-outsider-reportviewer/biochemistry-outsider-reportviewer.component.ts
+++ src/app/hims/lab/report-components/biochemistry-outsider-reportviewer/biochemistry-outsider-reportviewer.component.ts
@@ -4,33 +4,33 @@ import { Html } from "devexpress-reporting/dx-web-document-viewer";
 import { environment } from '../../../../../environments/environment';
 
 @Component({
-  selector: 'app-biochemistry-outsider-reportviewer',
-  templateUrl: './biochemistry-outsider-reportviewer.component.html',
-  styleUrls: ['./biochemistry-outsider-reportviewer.component.scss']
+    selector: 'app-biochemistry-outsider-reportviewer',
+    templateUrl: './biochemistry-outsider-reportviewer.component.html',
+    styleUrls: ['./biochemistry-outsider-reportviewer.component.scss']
 })
-export class BiochemistryOutsiderReportviewerComponent{
+export class BiochemistryOutsiderReportviewerComponent {
 
-  @ViewChild('scripts')
-  scripts: ElementRef;
+    @ViewChild('scripts')
+    scripts: ElementRef;
 
-  @ViewChild("control")
-  control: ElementRef
+    @ViewChild("control")
+    control: ElementRef
 
-  constructor(private renderer: Renderer2) { }
+    constructor(private renderer: Renderer2) { }
 
-  ngAfterViewInit() {
+    ngAfterViewInit() {
 
-      const reportUrl = ko["observable"]("BiochemistryOutsider"),
-          container = this.renderer.createElement("div");
-      container.innerHTML = Html;
-      var host = `${environment.repotr_url}`;
-      this.renderer.appendChild(this.scripts.nativeElement, container);
-      ko.applyBindings({
-          reportUrl,
-          requestOptions: {
-              host,
-              invokeAction: 'WebDocumentViewer/Invoke'
-          }
-      }, this.control.nativeElement);
-  }
+        const reportUrl = ko["observable"]("BiochemistryOutsider"),
+            container = this.renderer.createElement("div");
+        container.innerHTML = Html;
+        var host = `${environment.repotr_url}`;
+        this.renderer.appendChild(this.scripts.nativeElement, container);
+        ko.applyBindings({
+            reportUrl,
+            requestOptions: {
+                host,
+                invokeAction: 'WebDocumentViewer/Invoke'
+            }
+        }, this.control.nativeElement);
+    }
 }
--- src/app/hims/lab/report-components/biopsy-reportviewer/biopsy-reportviewer.component.ts
+++ src/app/hims/lab/report-components/biopsy-reportviewer/biopsy-reportviewer.component.ts
@@ -4,35 +4,35 @@ import { Html } from "devexpress-reporting/dx-web-document-viewer";
 import { environment } from '../../../../../environments/environment';
 
 @Component({
-  selector: 'app-biopsy-reportviewer',
-  templateUrl: './biopsy-reportviewer.component.html',
-  styleUrls: ['./biopsy-reportviewer.component.scss']
+    selector: 'app-biopsy-reportviewer',
+    templateUrl: './biopsy-reportviewer.component.html',
+    styleUrls: ['./biopsy-reportviewer.component.scss']
 })
-export class BiopsyReportviewerComponent{
+export class BiopsyReportviewerComponent {
 
 
-  @ViewChild('scripts')
-  scripts: ElementRef;
+    @ViewChild('scripts')
+    scripts: ElementRef;
 
-  @ViewChild("control")
-  control: ElementRef
+    @ViewChild("control")
+    control: ElementRef
 
-  constructor(private renderer: Renderer2) { }
+    constructor(private renderer: Renderer2) { }
 
-  ngAfterViewInit() {
+    ngAfterViewInit() {
 
-      const reportUrl = ko["observable"]("Biopsy"),
-          container = this.renderer.createElement("div");
-      container.innerHTML = Html;
-      var host = `${environment.repotr_url}`;
-      this.renderer.appendChild(this.scripts.nativeElement, container);
-      ko.applyBindings({
-          reportUrl,
-          requestOptions: {
-              host,
-              invokeAction: 'WebDocumentViewer/Invoke'
-          }
-      }, this.control.nativeElement);
-  }
+        const reportUrl = ko["observable"]("Biopsy"),
+            container = this.renderer.createElement("div");
+        container.innerHTML = Html;
+        var host = `${environment.repotr_url}`;
+        this.renderer.appendChild(this.scripts.nativeElement, container);
+        ko.applyBindings({
+            reportUrl,
+            requestOptions: {
+                host,
+                invokeAction: 'WebDocumentViewer/Invoke'
+            }
+        }, this.control.nativeElement);
+    }
 
 }

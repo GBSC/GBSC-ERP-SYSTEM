70e70f4d9302d07f3a4a7355a38451b6e090b840
kaleemshk <kalim29746@gmail.com>
2019-03-12 17:55:07 +0500

    update report

--- src/app/etracker/reports/shop-census-detail/shop-census-detail.component.ts
+++ src/app/etracker/reports/shop-census-detail/shop-census-detail.component.ts
@@ -13,12 +13,7 @@
 
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { StoreService } from '../../../../app/core/Services/ETracker/store.service';
-import { DxPivotGridModule, DxCheckBoxModule } from 'devextreme-angular';
-import PivotGridDataSource from 'devextreme/ui/pivot_grid/data_source';
 import { AuthService } from '../../../../app/core';
-
-
-import { HttpClient } from '@angular/common/http';
 import { AgGridNg2 } from 'ag-grid-angular';
 import "ag-grid-enterprise";
 
@@ -323,12 +318,16 @@ export class ShopCensusDetailComponent implements OnInit {
   
   setPrinterFriendly(api) {
     api.setDomLayout("print");
+    var eGridDiv = document.getElementById("#myGrid")
+    eGridDiv.style.width = "";  
+    eGridDiv.style.height = "";
   }
 
+
   setNormal(api) {
-    var eGridDiv = document.getElementById("#printableArea")
+    var eGridDiv = document.getElementById("#myGrid")
      eGridDiv.style.width = "1300px";
-    eGridDiv.style.height = "700px";
+    eGridDiv.style.height = "600px";
     api.setDomLayout(null);
   }
   
